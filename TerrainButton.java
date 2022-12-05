ebf5a6259fcafc9cf7b46f14cc64e95a840fd228
Joshua <buildingguy@sbcglobal.net>
2022-12-04 20:25:30 -0800

    TerrainButton constructor

--- TerrainButton.java
+++ TerrainButton.java
@@ -1,8 +1,16 @@
 import javax.swing.JButton;
+import java.awt.Dimension;
 
 class TerrainButton extends JButton {
     private int row = 0;
     private int col = 0;
     private boolean isGold;
     private boolean revealed;
+
+    public TerrainButton(int row, int col) {
+        this.row = row;
+        this.col = col;
+        Dimension size = new Dimension(50, 50);
+        setPreferredSize(size);
+    }
 }
