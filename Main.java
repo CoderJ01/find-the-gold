257ef19cfceaf20bd31f76f51897f6985ee13334
Joshua <buildingguy@sbcglobal.net>
2022-12-04 21:03:30 -0800

    JFrame test: pass

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.awt.*;
- import java.applet.Applet;
+import javax.swing.JPanel;
 
 public class Main extends JFrame {
     private static final int GRIDSIZE = 10;
@@ -17,6 +17,22 @@ public class Main extends JFrame {
     }
 
     private void initialize() {
+        JFrame frame = new JFrame();
+        frame.setTitle("GridLayout");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(800, 500);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(4, 5, 10, 10));
+
+        for(int i = 1; i <= 20; i++) {
+            JButton button = new JButton("Button" + Integer.toString(i));
+            panel.add(button);
+        }
         
+        frame.add(panel);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
