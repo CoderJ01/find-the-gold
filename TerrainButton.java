41b585eb9c0b9a6dd84c0bd107ccae38d5d46f99
Joshua <buildingguy@sbcglobal.net>
2022-12-04 20:20:27 -0800

    create TerrainButton fields

--- TerrainButton.java
+++ TerrainButton.java
@@ -1,5 +1,8 @@
 import javax.swing.JButton;
 
 class TerrainButton extends JButton {
-    
+    private int row = 0;
+    private int col = 0;
+    private boolean isGold;
+    private boolean revealed;
 }
