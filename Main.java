1e454c755f12493222f4c9f019e0b00282266dc8
Joshua <buildingguy@sbcglobal.net>
2022-12-04 20:10:00 -0800

    test window display: pass

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
+        new Main();
+    }
 
+    public Main() {
+        initialize();
+    }
+
+    private void initialize() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        setTitle("Find the Gold");
+        setSize(500, 500);
+        // setMinimunSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 }
