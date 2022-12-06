9c961e824b8e5616febb60252e0d12216c84e827
Joshua <buildingguy@sbcglobal.net>
2022-12-05 17:37:56 -0800

    fields and constructor for Player

--- Player.java
+++ Player.java
@@ -1,3 +1,11 @@
 public class Player {
-    
+    // fields
+    private final String PLAYER_NAME;
+    private int points;
+
+    // constructor
+    public Player(String name) {
+        PLAYER_NAME = name;
+        this.points = 5000;
+    }
 }
